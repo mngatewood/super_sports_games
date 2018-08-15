f7df54f71f699755a99caa21f00d0765b262d3c9
Michael Gatewood <michael@mngatewood.com>
2018-08-15 17:22:05 -0600

    Add standard_deviation_age method

--- lib/event.rb
+++ lib/event.rb
@@ -22,4 +22,15 @@ class Event
     (ages.sum.to_f / ages.count).round(1)
   end
 
+  def standard_deviation_age
+    # Subtract average age from each element of ages and return new array
+    ages_diff = ages.map { |i| (i - average_age).round(1) }
+    # Square each element in the previous array and return new array
+    ages_diff_squared = ages_diff.map { |i| (i * i).round(2) }
+    # Calculate the average of all elements in the previous array
+    ages_diff_squared_avg = ages_diff_squared.sum / ages.count
+    # Return the square root of the average calculated above
+    return Math.sqrt(ages_diff_squared_avg).round(2)
+  end
+
 end
