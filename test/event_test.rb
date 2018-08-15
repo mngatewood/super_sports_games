b0dce1e76066c69d296c2c94497f07c853be7c8a
Michael Gatewood <michael@mngatewood.com>
2018-08-15 17:43:29 -0600

    Add test_it_has_ages

--- test/event_test.rb
+++ test/event_test.rb
@@ -8,11 +8,15 @@ class EventTest < Minitest::Test
     event = Event.new("Curling", [24, 30, 18, 20, 41])
     assert_instance_of Event, event
   end
-  
+
   def test_it_has_a_name
     event = Event.new("Curling", [24, 30, 18, 20, 41])
     assert_equal "Curling", event.name
   end
 
+  def test_it_has_ages
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal [24, 30, 18, 20, 41], event.ages
+  end
 
 end
