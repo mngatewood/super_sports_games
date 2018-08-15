7b16f838408a1a4722049a3d01efad36ba4aceb7
Michael Gatewood <michael@mngatewood.com>
2018-08-15 17:46:16 -0600

    test_it_has_an_average_age

--- test/event_test.rb
+++ test/event_test.rb
@@ -29,4 +29,9 @@ class EventTest < Minitest::Test
     assert_equal 18, event.min_age
   end
 
+  def test_it_has_an_average_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 26.6, event.average_age
+  end
+
 end
