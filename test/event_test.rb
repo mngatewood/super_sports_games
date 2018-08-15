dedee65383d7188b425da317c92c0be8a1862731
Michael Gatewood <michael@mngatewood.com>
2018-08-15 17:48:45 -0600

    Add test_it_has_a_standard_deviation_age

--- test/event_test.rb
+++ test/event_test.rb
@@ -34,4 +34,9 @@ class EventTest < Minitest::Test
     assert_equal 26.6, event.average_age
   end
 
+  def test_it_has_a_standard_deviation_age
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 8.28, event.standard_deviation_age
+  end
+
 end
