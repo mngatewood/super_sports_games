4dedeba1923ee5976a5e4c68804183af0e6dfab4
Michael Gatewood <michael@mngatewood.com>
2018-08-15 18:30:26 -0600

    Add main_prompt_dialog method

--- lib/games_interface.rb
+++ lib/games_interface.rb
@@ -7,7 +7,30 @@ def initialize_session
   print "Enter the year of the games to begin: "
   year = gets.chomp
   games = Games.new(year)
-  puts games.year
+  main_prompt_dialog(games)
+end
+
+def main_prompt_dialog(games)
+  input = ""
+  while input != "A" && input != "V" && input != "X"
+    print "(A)dd an event, (V)iew the Summary of Events, or e(X)it. "
+    input = gets.chomp.upcase
+    if input == "A"
+      get_event_data(games)
+    elsif input == "V"
+      view_summary_dialog(games)
+    elsif input == "X"
+      exit
+    end
+  end
+end
+
+def get_event_data(games)
+  puts "Get event data"
+end
+
+def view_summary_dialog(games)
+  puts "View summary data"
 end
 
 initialize_session
